fb8ca5e9fa684b4970b372cacfbf544d22b58d11
waffleboi9000 <wafflebomb9000@gmail.com>
2024-01-04 14:28:59 -0500

    added discord rpc

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,6 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.cmd('autocmd VimEnter * Neotree')
+vim.api.nvim_set_keymap('n', '<leader>n', ':Neotree<CR>', { noremap = true, silent = true })
 return {}
